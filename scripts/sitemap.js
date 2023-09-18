3114a1c0178d119ccfde4d6c33aae959241e6052
Helena Hall <hougarry@gmail.com>
2023-09-18 19:13:10 +0800

    Update sitemap.js

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -1,23 +1,34 @@
+// Import necessary modules
 const path = require('path');
 const fs = require('fs-extra');
 const SitemapGenerator = require('sitemap-generator');
 
-const xmlFile = path.join(process.cwd(), '.deploy/sitemap.xml')
-const generator = SitemapGenerator('http://192.168.31.69:1987', {
-  maxDepth: 0,
-  filepath: xmlFile,
-  maxEntriesPerFile: 50000,
-  stripQuerystring: true,
-  decodeResponses: true,
+// Define the path where the sitemap file will be saved
+const xmlFile = path.join(process.cwd(), '.deploy/sitemap.xml');
+
+// Create a sitemap generator for your website
+const generator = SitemapGenerator('https://prompt.garyhou2023.info', {
+  maxDepth: 0,                // Maximum depth to crawl (0 means only the homepage)
+  filepath: xmlFile,          // Filepath to save the sitemap
+  maxEntriesPerFile: 50000,   // Maximum number of URLs per sitemap file
+  stripQuerystring: true,     // Remove query strings from URLs
+  decodeResponses: true,      // Decode responses to handle encoded URLs
 });
 
-// register event listeners
+// Register event listeners
 generator.on('done', async () => {
+  // Read the generated sitemap file
   const str = await fs.readFileSync(xmlFile);
-  const newStr = str.toString().replace(/http\:\/\/192\.168\.31\.69\:1987/g, 'https://wangchujiang.com/linux-command');
+
+  // Replace old domain with the same domain in the sitemap content
+  const newStr = str.toString().replace(/https:\/\/prompt\.garyhou2023\.info/g, 'https://prompt.garyhou2023.info');
+
+  // Save the modified sitemap content
   await fs.outputFile(xmlFile, newStr);
-  console.log('sitemaps created!')
+  
+  // Output a message indicating that sitemaps are created
+  console.log('Sitemaps created!');
 });
 
-// start the crawler
+// Start the sitemap generation process
 generator.start();
